180b57fd4959c11c8cc3bb7ca6e166ad77108e10
KukarskiySergey <Tyuiu.KukarskiySergey@outlook.com>
2024-12-28 10:24:48 +0500

    Task0

--- Tyuiu.KukarskiySA.Sprint6.Task0.V16.Lib/DataService.cs
+++ Tyuiu.KukarskiySA.Sprint6.Task0.V16.Lib/DataService.cs
@@ -1,7 +1,16 @@
-﻿namespace Tyuiu.KukarskiySA.Sprint6.Task0.V16.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
 
+namespace Tyuiu.KukarskiySA.Sprint6.Task0.V16.Lib
+{
+    public class DataService : ISprint6Task0V16
+    {
+        public double Calculate(int x)
+        {
+            // Формула расчета
+            double result = (1 + Math.Pow(x, 3)) / Math.Pow(x, 2);
+
+            // Округление до 3 знаков после запятой
+            return Math.Round(result, 3);
+        }
     }
 }
